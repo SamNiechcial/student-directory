3e37d83a0b3eedfc2845e331c485903375f94aff
Sam Niechcial <samniechcial@gmail.com>
2018-03-14 14:51:51 +0000

    Added variable and reduced line count in directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 ## first we print the list of students:
 puts "The students of Villains Academy"
 puts "-------------"
@@ -13,10 +14,8 @@ puts "The Joker"
 puts "Joffery Baratheon"
 puts "Norman Bates"
 # finally, we print the total number of students
-print "Overall, we have "
+print "Overall, we have #{student_count} great students"
 # it's important that print() doesn't add new line characters
-print 11
-puts " great students"
 # New lines can be added manually to print commands like so:
 # print "line1\nline2\nline3"
 # These \n escape characters need to be in double quotes!
