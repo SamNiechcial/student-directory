94a7272118cbcf57a3a2285a53bdff0d8619c0d8
Sam Niechcial <samniechcial@gmail.com>
2018-03-14 15:08:42 +0000

    Defined methods for directory.rb

--- directory.rb
+++ directory.rb
@@ -12,15 +12,22 @@ students = [
   "Joffery Baratheon",
   "Norman Bates"
 ]
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# finally, we print the total
-print "Overall, we have #{students.count} great students"
-
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
 # it's important that print() doesn't add new line characters
 # New lines can be added manually to print commands like so:
 # print "line1\nline2\nline3"
