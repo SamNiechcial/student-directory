94162b9c5a705fd3e926126de50c2fa96b11fbbd
Sam Niechcial <samniechcial@gmail.com>
2018-03-16 14:51:35 +0000

    Step 10: More refactoring completed

--- directory.rb
+++ directory.rb
@@ -1,26 +1,38 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students(students)
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-      end
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
   end
 end
 
-def input_students(students)
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def input_students
 
   puts "Please enter the student details"
 
@@ -28,10 +40,10 @@ def input_students(students)
 
     puts "Do you wish to stop? Enter 'Yes' to stop or anything else to enter another student's details:"
     stop = gets.chomp
-    if stop == "Yes" && students.length == 0
+    if stop == "Yes" && @students.length == 0
       puts "Student List not printed as no students have been entered"
       exit
-    elsif stop == "Yes" && students.length != 0
+    elsif stop == "Yes" && @students.length != 0
       break
     end
 
@@ -49,18 +61,18 @@ def input_students(students)
     puts "What are the student's hobbies?"
     hobbies = gets.chomp
 
-    students << {name: name, cohort: cohort, country: country, height: height, hobbies: hobbies}
+    @students << {name: name, cohort: cohort, country: country, height: height, hobbies: hobbies}
 
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
 
     puts "Did you make any errors with your last entry? Enter 'Yes' if so, or anything else if not:"
     error = gets.chomp
     if error == "Yes"
-      discard = students.pop
+      discard = @students.pop
       puts "Please re-enter the student data"
     end
   end
-  students
+  @students
 end
 
 def print_header
@@ -68,18 +80,18 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  sorted = students.sort_by! { |student_sort| student_sort[:cohort]}
+def print_students_list
+  sorted = @students.sort_by! { |student_sort| student_sort[:cohort]}
   sorted.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort) (born in #{student[:country]}) (height: #{student[:height]}) (Enjoys #{student[:hobbies]})".center(220)
   end
 end
 
-def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student"
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
